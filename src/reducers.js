88d7afc024c8cf1a3cf468834db21d0e3ec4c33b
peizixiang <peizixiang@sensetime.com>
2019-10-25 14:10:50 +0800

    dashboard Header

--- src/reducers.js
+++ src/reducers.js
@@ -3,12 +3,14 @@ import { routerReducer } from 'react-router-redux';
 import home from 'containers/Home/reducer';
 import tagPost from 'containers/TagPost/reducer';
 import article from 'containers/Article/reducer';
+import dashboard from 'containers/Dashboard/reducer';
 
 export default asyncReducers =>
   combineReducers({
     home,
     tagPost,
     article,
+    dashboard,
     routing: routerReducer,
     ...asyncReducers,
   });
